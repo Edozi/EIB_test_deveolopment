d18da0ccd0632bfac1fe978f9a65337536e5075b
Edozi <100856785+Edozi@users.noreply.github.com>
2025-08-08 16:07:56 +0300

    Create test_example.py

---
+++ test_example.py
@@ -0,0 +1,18 @@
+from langchain_community.document_loaders import TextLoader, PyPDFLoader, RecursiveUrlLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import OpenAIEmbeddings
+from langchain_unstructured import UnstructuredLoader
+
+
+loader = TextLoader("Data/Gorev.txt")
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap = 100)
+
+chunks = text_splitter.split_documents(documents)
+embeddings = OpenAIEmbeddings()
+vectorstore = InMemoryVectorStore.from_documents(chunks,embeddings)
+
+def search_query(query):
+    docs = vectorstore.similarity_search(query, k=1)
+    return docs[0].page_content
