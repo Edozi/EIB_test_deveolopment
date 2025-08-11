4b0d346ac7763925ddb1737af180f8ab5ba31f3c
Edozi <100856785+Edozi@users.noreply.github.com>
2025-08-11 10:58:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,24 @@ from test_example import search_query
 
 client = OpenAI()
 
-def chat(query):
-    context = search_query(query)
+def chat(query, chat_history):
+    context = search_webpage_final(query)
     prompt_with_context = f"Context: {context}\n\nUser input: {query}"
 
-    response = client.chat.completions.create( 
+    chat_history.append({"role": "user", "content": prompt_with_context})
+
+    response = client.chat.completions.create(
         model="gpt-4o-2024-08-06", 
-        messages=[{"role": "user", "content": prompt_with_context}],
+        messages=chat_history,
         stream=True
     )
 
+    complete_response = ""
+
     for chunk in response:
         if chunk.choices[0].delta.content is not None:
+            complete_response += chunk.choices[0].delta.content
             yield chunk.choices[0].delta.content
 
-test = "Ege Ihracatci Birliklerinde en guncel 5 duyuru ne hakkinda?"
+    chat_history.append({"role": "assistant", "content": complete_response})
 
